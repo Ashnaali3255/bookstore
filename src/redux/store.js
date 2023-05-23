86c953692151e25b3d9e884e4a18b70a32ff8cae
Ashnaali3255 <ashnaalipk@gmail.com>
2023-05-23 20:30:17 +0500

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+// store.js
+
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './booksSlice';
+import categoriesReducer from './categoriesSlice';
+
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
+});
+
+export default store;
