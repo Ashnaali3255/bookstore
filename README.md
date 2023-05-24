d57463089fa36773ff87ee162c78125fe4b920d5
Ashnaali3255 <ashnaalipk@gmail.com>
2023-05-24 21:34:40 +0500

    Add and remove books with react and redux

--- README.md
+++ README.md
@@ -33,8 +33,8 @@ JSX,CSS, Redux
 ## 🔭 Key Features <a name="key-features"></a>
 
 - [ ] **[Setup a Redux store]**
-- [ ] **[Create a slice to save your state and reducers]**
-- [ ] **[Add and Remove a book]**
+- [ ] **[Use store, actions and reducers in React.]**
+- [ ] **[Connect React and Redux.]**
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
 <!-- GETTING STARTED -->
--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,20 +1,45 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  books: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
+};
+
 const bookSlice = createSlice({
-  name: 'books',
-  initialState: [],
+  name: 'book',
+  initialState,
   reducers: {
     addBook: (state, action) => {
-      state.push(action.payload);
+      const book = {
+        item_id: `item${state.books.length + 1}`,
+        title: action.payload.title,
+        author: action.payload.author,
+      };
+      state.books.push(book);
     },
-    removeBook: (state, action) => {
-      const index = state.findIndex((book) => book.id === action.payload);
-      if (index !== -1) {
-        state.splice(index, 1);
-      }
-    },
+    removeBook: (state, action) => ({
+      books: state.books.filter((book) => book.item_id !== action.payload.id),
+    }),
   },
 });
 
-export const { addBook, removeBook } = bookSlice.actions;
 export default bookSlice.reducer;
+export const { addBook, removeBook } = bookSlice.actions;
